accb21582cfc520e74e5701be428961f154fdd9d
hosthabib <88926306+hosthabib@users.noreply.github.com>
2023-09-10 17:50:06 +0530

    User interface of banking system
    Upload frontend in docs

---
+++ doc/footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		.footer {
+      border-top:3px solid green;
+  padding: 5px; /* Some padding */
+  text-align: center; /* Center text*/
+  background: #00ffcc; /* Grey background */
+  height:213px;
+}
+.fa {
+  padding: 5px;
+  font-size: 30px;
+  width:40px; 
+  float:center;
+  text-align:center;
+  text-decoration: none;
+  margin: 4px 10px;
+  border-radius:45%;
+}
+.fa:hover {
+    opacity:1.4;
+}
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+.fa-instagram {
+  background: #125688;
+  color: white;
+}
+.fa-github{
+  background: #125688;
+  color: white;
+}
+.fa-linkedin{
+  background: #125688;
+  color: white;
+}
+.fa-youtube{
+  background: #125688;
+  color: white;
+}
+	</style>
+}
+</head>
+<body>
+<div class="footer">
+      <h4>Design and coded by Habib Ullah</h4>
+  <h2>Follow Us on</h2>
+  <a href="https://facebook.com" class="fa fa-facebook"></a>
+  <a href="https://instagram.com" class="fa fa-instagram"></a>
+  <a href="https://github.com/hosthabib" class="fa fa-github"></a>
+  <a href="https://www.linkedin.com/in/habib-ullah-ansari-8133041ab/" class="fa fa-linkedin"></a>
+  <a href="https://m.youtube.com/channel/UC_WErTc4MjHGqNzSUjubprQ/featured" class=" fa fa-youtube"></a>
+  <h4> No Copyright 2021. © All Rights Reserved<br>
+  	Privacy Policy-Terms and Conditions
+  </h4>
+</div>
+</body>
+</html>
